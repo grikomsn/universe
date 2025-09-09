c7c1e2ebac97d3f5da8631a2cf106dedea097d76
Griko Nibras <griko@nibras.co>
2025-09-09 21:01:02 +0700

    chore: remove redundand brew setup

--- .config/fish/conf.d/01-brew.fish
+++ .config/fish/conf.d/01-brew.fish
@@ -7,13 +7,6 @@ end
 
 # https://github.com/orgs/Homebrew/discussions/4412#discussioncomment-8651316
 if test -d $HOMEBREW_PREFIX
-    set -gx HOMEBREW_CELLAR "$HOMEBREW_PREFIX/Cellar"
-    set -gx HOMEBREW_REPOSITORY "$HOMEBREW_PREFIX/homebrew"
-    fish_add_path -gP "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
-    ! set -q MANPATH; and set MANPATH ''
-    set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
-    ! set -q INFOPATH; and set INFOPATH ''
-    set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
     $HOMEBREW_PREFIX/bin/brew shellenv | source
 
     # bin extras
