72c938c2c5adb4ea4cf511277847c55c867bac98
Griko Nibras <griko@nibras.co>
2025-10-13 07:09:17 +0700

    scripts: update

--- scripts/cursor.sh
+++ scripts/cursor.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
+# get extensions from brewfile
 readarray -t EXTENSIONS < <(curl -fsSL https://universe.nibras.co/Brewfile | grep '^vscode' | awk '{print $2}' | sed 's/^"\(.*\)"$/\1/')
 
+# symlink cursor to others
+mkdir -p ~/.cursor
+ln -sf ~/.cursor ~/.vscode
+ln -sf ~/.cursor ~/.vscode-oss
+
+# symlink cursor configs
+if [[ "$(uname -o)" == "Darwin" ]]; then
+  CODE_DATA_DIR=~/Library/Application\ Support/Code
+  CURSOR_DATA_DIR=~/Library/Application\ Support/Cursor
+else
+  CODE_DATA_DIR=~/.config/Code
+  CURSOR_DATA_DIR=~/.config/Cursor
+fi
+mkdir -p $CODE_DATA_DIR
+ln -sf $CURSOR_DATA_DIR/User/settings.json $CODE_DATA_DIR/User/settings.json
+ln -sf $CURSOR_DATA_DIR/User/keybindings.json $CODE_DATA_DIR/User/keybindings.json
+
+# install extensions via code
 if command -v code &>/dev/null; then
   for extension in "${EXTENSIONS[@]}"; do
     code --install-extension "$extension"
@@ -11,6 +30,7 @@ else
   exit 1
 fi
 
+# install extensions again but via cursor
 if command -v cursor &>/dev/null; then
   for extension in "${EXTENSIONS[@]}"; do
     cursor --install-extension "$extension"
