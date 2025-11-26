82728804b6999845360b553330daf32c4347a717
Griko Nibras <griko@nibras.co>
2025-11-27 04:03:18 +0700

    feat: ast-grep via pnpm global on linux

--- .config/fish/functions/node-update-globals.fish
+++ .config/fish/functions/node-update-globals.fish
@@ -5,5 +5,8 @@ function node-update-globals --description 'update node related package managers
     corepack prepare --activate pnpm@latest
     corepack prepare --activate yarn@1.22.22
     set -l NODE_GLOBAL_PACKAGES @biomejs/biome eas-cli fish-lsp neovim prettier serve tsx vercel
+    if string match -q Linux (uname -s)
+        set -a NODE_GLOBAL_PACKAGES @ast-grep/cli
+    end
     pnpm --global add $NODE_GLOBAL_PACKAGES
 end
