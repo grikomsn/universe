9d498433bb78836d1c7ea8ddff4683072bfd4e2f
Griko Nibras <griko@nibras.co>
2025-12-10 00:51:22 +0700

    chore: script whitespaces

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,9 @@
 
 sudo -v
 while true; do
-    sudo -n true
-    sleep 60
-    kill -0 "$$" || exit
+	sudo -n true
+	sleep 60
+	kill -0 "$$" || exit
 done 2>/dev/null &
 
 mkdir -p ~/Projects ~/Scripts ~/Temporary ~/Workspace ~/.cursor
@@ -12,30 +12,30 @@ ln -sf ~/.cursor ~/.vscode
 ln -sf ~/.cursor ~/.vscode-oss
 
 if [[ "$(uname -o)" == "Darwin" ]]; then
-    xcode-select --install
-    read -p "Press [Enter] key after Xcode Command Line Tools are installed..."
+	xcode-select --install
+	read -p "Press [Enter] key after Xcode Command Line Tools are installed..."
 
-    if [[ "$(uname -m)" == "arm64" ]]; then
-        softwareupdate --install-rosetta --agree-to-license
-        read -p "Press [Enter] key after Rosetta is installed..."
-    fi
+	if [[ "$(uname -m)" == "arm64" ]]; then
+		softwareupdate --install-rosetta --agree-to-license
+		read -p "Press [Enter] key after Rosetta is installed..."
+	fi
 
-    sudo xcodebuild -license accept
-    sudo xcodebuild -runFirstLaunch
+	sudo xcodebuild -license accept
+	sudo xcodebuild -runFirstLaunch
 
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-    if [[ "$(uname -m)" == "arm64" ]]; then
-        eval $(/opt/homebrew/bin/brew shellenv)
-    else
-        eval $(/usr/local/bin/brew shellenv)
-    fi
+	curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+	if [[ "$(uname -m)" == "arm64" ]]; then
+		eval $(/opt/homebrew/bin/brew shellenv)
+	else
+		eval $(/usr/local/bin/brew shellenv)
+	fi
 
-    curl -fsSL https://universe.nibras.co/Brewfile >~/.Brewfile
-    brew bundle --global
+	curl -fsSL https://universe.nibras.co/Brewfile >~/.Brewfile
+	brew bundle --global
 fi
 
 if command -v mkcert >/dev/null 2>&1; then
-    mkcert -install
+	mkcert -install
 fi
 
 curl -sSL https://raw.githubusercontent.com/yarlson/lnk/main/install.sh | bash
@@ -43,10 +43,10 @@ lnk init -r https://github.com/grikomsn/universe.git
 lnk pull
 
 if [[ "$(uname -o)" == "Darwin" ]]; then
-    lnk pull -H darwin
+	lnk pull -H darwin
 fi
 if [[ "$(uname -s)" == "Linux" ]]; then
-    lnk pull -H linux
+	lnk pull -H linux
 fi
 
 curl -fsSL https://bun.com/install | bash
