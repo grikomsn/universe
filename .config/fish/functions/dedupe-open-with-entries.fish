506d7160d6aa6cef49ea6f6bdcf6325a8f0725a6
Griko Nibras <griko@nibras.co>
2026-02-24 00:10:38 +0700

    fix(fish): update dedupe-open-with-entries

--- .config/fish/functions/dedupe-open-with-entries.fish
+++ .config/fish/functions/dedupe-open-with-entries.fish
@@ -1,8 +1,8 @@
 function dedupe-open-with-entries --description 'dedupe open with entries'
-    if (uname -o) = Darwin
-        /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+    if test (uname -o) = Darwin
+        /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -r -domain local -domain system -domain user
     else
         echo "Not supported on this platform"
-        exit 1
+        return 1
     end
 end
